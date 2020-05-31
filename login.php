3984eea14a9f373101f6d194579b634ae9963627
rungo728 <rufii72865@gmail.com>
2020-05-31 22:45:48 +0900

    Update login.php
    ログイン成功・失敗時における処理とエラー表示を記述

--- login.php
+++ login.php
@@ -1,3 +1,46 @@
+<?php
+// NOTICEエラーを非表示にする記述
+error_reporting(E_ALL & ~E_NOTICE);
+?>
+<!-- データベースに接続するためプログラム -->
+<?php
+session_start();
+require('dbconnect.php');
+// ログインボタンが押されて呼び出されたのかどうかの判断を記述
+// $_POSTが空でなければ
+if(!empty($_POST)){
+  // データベースから情報を引っ張ってくる
+  if ($_POST['email']!== '' && $_POST['password'] !== ''){
+    // データベースにお問い合わせ
+    $login = $db->prepare('SELECT * FROM members WHERE email=? AND password=?');
+    // 次にemail=?とpassword=?の部分に登録された情報を指定すればデータベースに問い合わせすることができる
+    $login->execute(array(
+      $_POST['email'],
+      // 入力したパスワードを暗号化する
+      sha1($_POST['password'])
+    ));
+    // データが返ってきていればログイン成功
+    $member = $login->fetch();
+    // ログインが成功していれば
+    if ($member){
+      // ログインした情報をsession変数に保存しておくため
+      $_SESSION['id'] = $member['id'];
+      $_SESSION['time'] = time();
+      header('Location: index.php');
+      exit();
+    }else{
+      // ここで一度記録しておいてログイン画面上でエラー表示させる
+      $error['login'] = 'failed';
+    }
+  }else{
+    // メールアドレス、パスワードどちらかが空である場合
+    $error['login'] = 'blank';
+  }
+
+}
+?>
+
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -21,11 +64,17 @@
       <dl>
         <dt>メールアドレス</dt>
         <dd>
-          <input type="text" name="email" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['email']); ?>" />
+          <input type="text" name="email" size="35" maxlength="255" value="<?php print (htmlspecialchars($_POST['email'],ENT_QUOTES)); ?>" />
+          <?php if($error['login'] === 'blank'):?>
+            <p class="error">メールアドレスとパスワードをご記入ください</p>
+          <?php endif; ?>
+          <?php if($error['login'] === 'failed'):?>
+            <p class="error">ログインに失敗しました。正しくご記入ください</p>
+          <?php endif; ?>
         </dd>
         <dt>パスワード</dt>
         <dd>
-          <input type="password" name="password" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['password']); ?>" />
+          <input type="password" name="password" size="35" maxlength="255" value="<?php print (htmlspecialchars($_POST['password'],ENT_QUOTES)); ?>" />
         </dd>
         <dt>ログイン情報の記録</dt>
         <dd>
