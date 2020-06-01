bc9986f4c2f36e3d783460cbb5d043ff3393ae91
rungo728 <rufii72865@gmail.com>
2020-06-01 15:32:29 +0900

    member list display  
    ログインメンバーが投稿した内容が一覧で出るようにする

--- index.php
+++ index.php
@@ -73,17 +73,20 @@ $posts = $db->query('SELECT m.name,m.picture, p.* FROM members m,posts p WHERE m
         </p>
       </div>
     </form>
-
+<!-- 配列の中身を精査していき、最後まで$postsから$postに繰り返し代入される -->
+<?php foreach($posts as $post):?>
     <div class="msg">
-    <img src="member_picture" width="48" height="48" alt="" />
-    <p><span class="name">（）</span>[<a href="index.php?res=">Re</a>]</p>
-    <p class="day"><a href="view.php?id="></a>
+    <img src="member_pictures/<?php print(htmlspecialchars($post['picture'],ENT_QUOTES));?>" width="48" height="48" alt="" />
+    <!-- 変数$postの中からメッセージ部分を表示させる -->
+    <p><?php print(htmlspecialchars($post['message'],ENT_QUOTES));?><span class="name">（<?php print(htmlspecialchars($post['name'],ENT_QUOTES));?>）</span>[<a href="index.php?res=">Re</a>]</p>
+    <p class="day"><a href="view.php?id="><?php print(htmlspecialchars($post['created'],ENT_QUOTES));?></a>
 <a href="view.php?id=">
 返信元のメッセージ</a>
 [<a href="delete.php?id="
 style="color: #F33;">削除</a>]
     </p>
     </div>
+<?php endforeach; ?>
 
 <ul class="paging">
 <li><a href="index.php?page=">前のページへ</a></li>
