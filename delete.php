720fa1c6fb6108bab730c8f476ee59051988158c
rungo728 <rufii72865@gmail.com>
2020-06-02 09:44:30 +0900

    Create delete.php
    削除ボタンを押すと一覧とデータベースから削除されるようにした。

---
+++ delete.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+require('dbconnect.php');
+// 自分のメッセージを削除する
+if (isset($_SESSION['id'])){
+  $id = $_REQUEST['id'];
+
+  $messages = $db->prepare('SELECT * FROM posts WHERE id=?');
+  // urlに渡されたidを受け取って$messagesに入れる
+  $messages->execute(array($id));
+  $message = $messages->fetch();
+  if ($message['member_id'] == $_SESSION['id']){
+    $del = $db->prepare('DELETE FROM posts WHERE id=?');
+    $del->execute(array($id));
+  }
+
+}
+header('Location: index.php');
+exit();
+?>
