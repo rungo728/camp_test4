2c9e31ba20ffe6f58ec0a6fc281e2cef8a3c4199
rungo728 <rufii72865@gmail.com>
2020-06-02 22:16:43 +0900

    small fix

--- login.php
+++ login.php
@@ -58,6 +58,7 @@ if(!empty($_POST)){
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" href="css/reset.css" />  
 <link rel="stylesheet" type="text/css" href="css/style.css" />
 <title>ログインする</title>
 </head>
